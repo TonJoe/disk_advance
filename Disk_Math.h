6140fb5c9649aed4d8c604f6b017cf0fe465f59e
TonJoe <tuz38@psu.edu>
2017-07-02 14:04:55 -0400

    Changes to be committed: 	new file:   Disk_Math.h

---
+++ Disk_Math.h
@@ -0,0 +1,9 @@
+#define PI 3.14159265358979323846
+#define DISK 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<complex>
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
