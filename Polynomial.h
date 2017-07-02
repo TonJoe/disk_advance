8609523cc7ea474f82a37859e518c54b5357ff12
TonJoe <tuz38@psu.edu>
2017-07-02 12:18:08 -0400

    Changes to be committed: 	modified:   Polynomial.h Added Derivative

--- Polynomial.h
+++ Polynomial.h
@@ -7,6 +7,7 @@
 #include<cstdlib>
 #include<time.h>
 #include<algorithm>
+#include<iostream>
 using namespace std;
 typedef complex<double> Cdouble;
 
@@ -15,20 +16,23 @@ class Term	//Term in a polynomial.
 {
 	friend Polynomial;
 public:
-	double coef;
+	Cdouble coef;
 	int exp;
 };
 class Polynomial
 {
-	friend ostream & operator<<(ostream &o,const Polynomial & poly);  
+	friend ostream & operator<<(ostream &o,const Polynomial & poly);
+	//friend Polynomial Der(const Polynomial & poly, int n);
 public:
 	Polynomial();
 	Polynomial(const Polynomial & poly);
 	~Polynomial();
 	Polynomial operator+(const Polynomial & poly); //Polynomial addition.
-	Polynomial operator*(const Polynomial & poly);	
+	Polynomial operator*(const Polynomial & poly);
+	Polynomial Deriv(int n);
+	//void operator=(const Polynomial & poly);
 	Cdouble Eval(Cdouble z);	//Evaluate the polynomial;
-	void NewTerm(double coef, int exp);
+	void NewTerm(Cdouble coef, int exp);
 private:
 	void InsertTerm(const Term & term);
 private:
@@ -44,7 +48,7 @@ Polynomial::Polynomial()	//Initializaing a polynomial with 10 terms.
 	termArray= new Term[this->capacity];
 }
 
-Polynomial::Polynomial(const Polynomial & b)  
+Polynomial::Polynomial(const Polynomial & b)  //Initializaing a polynomial copying from b
 {  
     this->terms=0;  
     this->capacity=b.capacity;  
@@ -67,8 +71,8 @@ Polynomial Polynomial::operator+(const Polynomial & b)
     int bPos=0;  
     while(aPos<terms && bPos<b.terms){  
         if(termArray[aPos].exp == b.termArray[bPos].exp){  
-            float coef=termArray[aPos].coef+b.termArray[bPos].coef;  
-            if(coef)c.NewTerm(coef,termArray[aPos].exp);  
+            Cdouble coef=termArray[aPos].coef+b.termArray[bPos].coef;  
+            if(norm(coef)>0.00001)c.NewTerm(coef,termArray[aPos].exp);  
             aPos++;bPos++;  
         }else if(termArray[bPos].exp < b.termArray[bPos].exp){  
             c.NewTerm(b.termArray[bPos].coef,b.termArray[bPos].exp);  
@@ -88,19 +92,42 @@ Polynomial Polynomial::operator+(const Polynomial & b)
     }  
     return c;  
 }  
+Polynomial Polynomial::Deriv(int n)
+{
+	Polynomial c;
+	//int aPos=0;
+	int bPos=0;
+	while(bPos<this->terms)
+	{
+		Cdouble bexp(this->termArray[bPos].exp,0);
+		Cdouble coef(this->termArray[bPos].coef*bexp);
+		if(norm(coef)>0.0000001)
+		{
+			c.NewTerm(coef,this->termArray[bPos].exp-1);
+			bPos++;
+		}
+	}
+	return c;
+}
+
 Polynomial Polynomial::operator*(const Polynomial & b)  
 {  
     Polynomial c;  
     for(int i=0; i<terms; i++){  
         for(int j=0; j<b.terms; j++){  
-            float coef = termArray[i].coef*b.termArray[j].coef;  
+            Cdouble coef = termArray[i].coef*b.termArray[j].coef;  
             int exp = termArray[i].exp + b.termArray[j].exp;  
             c.NewTerm(coef,exp);  
         }  
     }  
     return c;  
 }
-void Polynomial::NewTerm(double coef, int exp)  
+
+/**void Polynomial::operator=(const Polynomial & poly)
+{
+	
+}**/
+void Polynomial::NewTerm(Cdouble coef, int exp)  
 {  
     if(terms == capacity){  
         capacity *= 2;  
@@ -120,7 +147,7 @@ void Polynomial::InsertTerm(const Term & term)
     }  
     if(term.exp == termArray[i].exp){  
         termArray[i].coef += term.coef;  
-        if(!termArray[i].coef){  
+        if(termArray[i].coef!=polar(0.,0.)){  
             for(int j=i; j<terms-1; j++)  
                 termArray[j]= termArray[j+1];  
             terms--;  
@@ -140,3 +167,16 @@ Cdouble Polynomial::Eval(Cdouble z)
     }  
     return res;  
 }
+
+ostream & operator<<(ostream & o,const Polynomial & poly)  
+{  
+    for(int i=0;i<poly.terms-1;i++){  
+        o<<poly.termArray[i].coef<<"x^"<<poly.termArray[i].exp<<" + ";  
+    }  
+    o<<poly.termArray[poly.terms-1].coef<<"x^"<<poly.termArray[poly.terms-1].exp;  
+    return o;  
+}
+
+/********************************/
+/***Other stuff
+/********************************/
